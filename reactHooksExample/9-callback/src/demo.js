c6291536cbd4a61ee094079dedfa717e06243fd7
huangquanya <1036871949@qq.com>
2020-08-16 21:49:48 +0800

    lemon-9录制

--- reactHooksExample/9-callback/src/demo.js
+++ reactHooksExample/9-callback/src/demo.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React,{useState, memo, useCallback} from 'react'
 
 export const MyComponent = () =>{
-    
+    const [username, setUsername] = useState("John")
+    const [lastname, setLastname] =useState("Do")
+
+    const resetName = useCallback(() => setUsername(""),[]);
+
+    return (
+        <>
+            <h3>
+                {username}-{lastname}
+            </h3>
+            <input value={username} onChange={e=>setUsername(e.target.value)}  />
+            <input value={lastname} onChange={e=>setLastname(e.target.value)}  />
+            <ResetValue onRest={resetName} >重置姓名</ResetValue>
+        </>
+    )
 }
 
+const ResetValue = memo(props=>{
+    console.log("应该只在第一次渲染时输出一次，如多次输出，检查是否成功缓存")
+    return (
+        <button onClick={props.onRest}>重置姓名</button>
+    )
+}) 

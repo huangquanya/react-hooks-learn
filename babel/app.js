887e95ae71a111793c37f38ec75f6c66646f29dc
huangquanya <1036871949@qq.com>
2020-08-16 21:49:21 +0800

    babel学习

---
+++ babel/app.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react'
+
+function observer(){
+    
+}
+
+@observer
+class Re extends Component{
+    render(){
+        return (
+            <div>
+                <img src="baidu.com" alt="baidu"/>
+            </div>
+        )
+    }
+}
+
+export default Re;
---
+++ babel/webpack.config.js
@@ -0,0 +1,20 @@
+const config = {
+    entry:'./src/index.js',
+    resolve: {
+        extensions: ['.js','.jsx']
+    },
+    devServer:{
+        contentBase:'./dist',
+        hot:true
+    },
+    module: {
+        rules: [
+            {
+                test:/(.js|.jsx)$/,
+                use:['babel-loader'],
+                exclude: /node_modules/
+            }
+        ]
+    }
+}
+module.exports = config

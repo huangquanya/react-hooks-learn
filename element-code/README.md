8d82dfcc7f4522efba96cab13ad20213e8878a3a
huangquanya <1036871949@qq.com>
2020-08-25 22:12:45 +0800

    vue+elementUI尝试

---
+++ element-code/README.md
@@ -0,0 +1,16 @@
+- vue-cli 4.x  vue 3.0 变化太大，拥抱react
+- vue create project-name
+    vue 2.x vue init webpack project-name  传统，现在流
+    vue 3.x vue create project-name
+    - ts
+    - PWA Progressive Web App(PWA) Support 渐进式网页应用
+    - Router Vuex 全家桶
+    - CSS Pre-processors stylus vw autoprefixer
+        编译 sass/stylus 插件系统 autoprefixer前缀 
+        postcss-px-to-viewport  cssnext
+    - linter 代码风格确保文件 ；"" '' tab
+        ESLint 保证大家代码风格一样
+        不用的变量不要定义，要删除干净
+        对齐代码"" ''
+        js的结尾不加;
+    - unit Testing E2E 测试
---
+++ element-code/src/components/ElButton.vue
@@ -0,0 +1,50 @@
+<template>
+    <Button 
+    @click="handleClick"
+    class="el-button"
+    :class="[
+    type?'el-button--'+type:'',
+    {
+        'is-round':round,
+        'is-circle':circle,
+        'is-plain':plain
+    }
+    ]"
+    >
+        {{type}}-{{tit}}
+        <slot></slot>
+    </Button>
+</template>
+<script>
+// props检查 解构出来 
+// vue 怎么做
+export default {
+    data(){
+        return {
+            tit:'hello'
+        }
+    },
+    props: {
+        type: {
+            type:String,
+            default: 'default'
+        },
+        round: Boolean,
+        plain: Boolean,
+        circle: Boolean
+    },
+    created(){
+        console.log(this.type,this.tit);
+        
+    },
+    methods: {
+        handleClick(evt) {
+            this.$emit('click', evt);
+        }
+    }
+}
+</script>
+
+<style lang="stylus">
+
+</style>
